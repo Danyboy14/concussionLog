396fa76111a64f3e2a5d29c1a4ac717f388030ad
Danyboy14 <dowen@tcd.ie>
2017-09-07 15:58:45 +0100

    Update db.php

--- Web/db.php
+++ Web/db.php
@@ -1,6 +1,6 @@
 <?php
    // Enter your Host, username, password, database below.
-   $con = mysqli_connect("mysql11.000webhost.com","a3574765_Dan21","Danyboy14","a3574765_logins");
+   $con = mysqli_connect("mysql11.000webhost.com","id2842415_dowen","Danyboy14","id2842415_concussionlog");
    // Check if error in connection
    if (mysqli_connect_errno())
    {
